3b474b98a503aa31a46e87ecc0d41f08e2a09171
dbagley23 <dale@reparteegallery.com>
2015-07-31 18:04:31 -0600

    sf

---
+++ Scott-teaching.js
@@ -0,0 +1,12 @@
+// CLIENT
+$.ajax({
+    url: 'http://somewhere.com/some/path', //   step 1
+    METHOD: 'GET',
+    success: function(response){ //STEP 3
+        //respopnse equals 'something back'
+    }
+})
+
+GET
+PUT REPLACES
+POST NEW OBJECT
